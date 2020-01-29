c65f30393bd3d3c4509ed756f3e2e9d44dffb5c9
rahul-vyavahare <60432569+rahul-vyavahare@users.noreply.github.com>
2020-01-29 19:39:02 +0530

    Update substring.py

--- substring.py
+++ substring.py
@@ -2,13 +2,15 @@ x=input("enter string:")
 y=input("enter substring:")
 s,i,j=[],0,0
 while(i<len(x)):
-    if (i+j)<len(x)-1:
+    if (i+j)<len(x)-1 and j<len(y):
         if x[i+j]!=y[j]:
             i+=1
             j=0
             continue
-    if j<len(y):
-        j+=1
+        if j<len(y):
+            j+=1
+        elif j==len(y):
+            break
     else:
         break
 if j==len(y):
